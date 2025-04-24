65994745b20d9d7b0ef1296d5b051f397394c41a
Marcel Behar Fernández <marcel.behar@gmail.com>
2025-04-24 18:01:08 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(fileURLToPath(import.meta.url), './src'),
-    },
   },
 });
