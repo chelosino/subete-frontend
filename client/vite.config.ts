8626de9c4a905583c241a8d63ccf47931389e849
Marcel Behar Fernández <marcel.behar@gmail.com>
2025-04-24 00:31:45 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    port: 5173,
-  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
